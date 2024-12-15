9132a05161a1f24619803e80e447d69e675bb0c7
superman <annagu.kennedy@gmail.com>
2024-12-15 19:35:56 +0100

    no cookies

--- src/App.tsx
+++ src/App.tsx
@@ -17,46 +17,48 @@ const App = () => {
   const closeFaqs = () => setOpenFaqs(false);
 
   return (
-    <div className="app-container">
-      <div
-        style={{
-          position: "absolute",
-          width: "100%",
-          height: "100%",
-          background: "linear-gradient(145deg, black, #0a2436, #124672)",
-          zIndex: -2,
-        }}
-      />
-
-      <div className=" rounded w-28 h-28 ">
-        <img
-          className="rounded-full w-full bg-center h-full object-cover"
-          src={pageLogo}
-          alt="Logo"
+    <div className="h-full w-full">
+      <div className="app-container">
+        <div
+          style={{
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+            background: "linear-gradient(145deg, black, #0a2436, #124672)",
+            zIndex: -2,
+          }}
         />
-      </div>
 
-      <h2 className="text-slate-200 text-3xl font-bold my-5">
-        {" "}
-        Connect to claim
-      </h2>
+        <div className=" rounded w-28 h-28 ">
+          <img
+            className="rounded-full w-full bg-center h-full object-cover"
+            src={pageLogo}
+            alt="Logo"
+          />
+        </div>
 
-      <button className="button" onClick={() => setOpenWallet(true)}>
-        Connect Wallet
-      </button>
-
-      <div className="faq-container">
-        <button
-          onClick={() => setOpenFaqs(true)}
-          className="px-5 py-2 rounded bg-[#18181b] text-white font-bold cursor-pointer "
-        >
+        <h2 className="text-slate-200 text-3xl font-bold my-5">
           {" "}
-          FAQ's
+          Connect to claim
+        </h2>
+
+        <button className="button" onClick={() => setOpenWallet(true)}>
+          Connect Wallet
         </button>
+
+        <div className="faq-container">
+          <button
+            onClick={() => setOpenFaqs(true)}
+            className="px-5 py-2 rounded bg-[#18181b] text-white font-bold cursor-pointer "
+          >
+            {" "}
+            FAQ's
+          </button>
+        </div>
+        <ParticlesComponent />
+        <WalletConnect openWallet={openWallet} closeWallet={closeWallet} />
+        <Faqs openFaqs={openFaqs} closeFaqs={closeFaqs} />
       </div>
-      <ParticlesComponent />
-      <WalletConnect openWallet={openWallet} closeWallet={closeWallet} />
-      <Faqs openFaqs={openFaqs} closeFaqs={closeFaqs} />
     </div>
   );
 };
