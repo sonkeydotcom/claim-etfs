f5cc68de0e30ad3d8582a51138cae0513338de43
Kennedy Sunday <annagu.kennedy@gmail.com>
2025-02-17 02:13:44 +0100

    Update wallet-connect.tsx

--- src/components/wallet-connect.tsx
+++ src/components/wallet-connect.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Modal from "./modal";
-import axios from "axios";
+// import axios from "axios";
 import { useNavigate } from "react-router";
 
 interface WalletConnectProps {
